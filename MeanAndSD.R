87e535956675365deecec4fbfef290ffdd6d1c03
jrosario22 <43249854+jrosario22@users.noreply.github.com>
2018-12-13 20:47:36 -0500

    Updated

--- MeanAndSD.R
+++ MeanAndSD.R
@@ -1,16 +1,20 @@
-> library(readxl)
-> X2006_2012_Math_Test_Results_Citywide_SWD <- read_excel("C:/Users/jeson/Downloads/2006_-_2012_Math_Test_Results_-_Citywide_-_SWD.xlsx")
-> View(X2006_2012_Math_Test_Results_Citywide_SWD)
-> mathTest <- (X2006_2012_Math_Test_Results_Citywide_SWD)
-> mean(mathTest$`Number Tested`)
-[1] 61359.24
-> mean(mathTest$`Mean Scale Score`)
-[1] 661.949
-> mean(mathTest$`Pct Level 3 and 4`)
-[1] 52.1102
-> sd(mathTest$`Number Tested`)
-[1] 85528.53
-> sd(mathTest$`Mean Scale Score`)
-[1] 23.04293
-> sd(mathTest$`Pct Level 3 and 4`)
-[1] 23.02879
+X2006_2012_Math_Test_Results_Citywide_SWD <- read.csv("C:/Users/jeson/Downloads/2006_-_2012_Math_Test_Results_-_Citywide_-_SWD.csv")
+mathTest <- (X2006_2012_Math_Test_Results_Citywide_SWD)
+
+mean(mathTest$Number.Tested)
+# 35792.89
+
+mean(mathTest$Mean.Scale.Score)
+# 661.9167
+ 
+mean(mathTest$Pct.Level.3.and.4)
+# 52.09881
+
+sd(mathTest$Number.Tested)
+# 23682.64
+
+sd(mathTest$Mean.Scale.Score)
+# 23.42751
+
+sd(mathTest$Pct.Level.3.and.4)
+# 23.37023
